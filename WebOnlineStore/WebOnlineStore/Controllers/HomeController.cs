4bba56c31d90ea8997e6e8ed916bd7a2a97e8cd4
Quang Nguyen <nguyenbaquang226@gmail.com>
2020-05-08 16:12:53 +0700

    update frontend

--- WebOnlineStore/WebOnlineStore/Controllers/HomeController.cs
+++ WebOnlineStore/WebOnlineStore/Controllers/HomeController.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebOnlineStore.Models;
+using System.Data.SqlClient;
+using PagedList;
+using PagedList.Mvc;
 
 namespace WebOnlineStore.Controllers
 {
+    
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private MyDB db = new MyDB();
+        public ActionResult Index(string keyword, int? page)
         {
-            return View();
-        }
-        public ActionResult Contact()
-        {
-            return View();
+            int pageNumber = (page ?? 1);
+            int itemsPerPage = 12;
+
+            var products = db.SANPHAMs.ToList();
+            var pageSize = products.Count / itemsPerPage;
+            // Tất cả sp 
+            var spall = products
+                         .Where(p => p.ID_SP != null )
+                         .OrderBy(p => p.tensanpham)
+                         .ToList();
+            // sản phẩm bán chạy
+            var sp = spall.Where(p => p.hot == 1);
+
+            if (!string.IsNullOrEmpty(keyword) && !string.IsNullOrWhiteSpace(keyword))
+            {
+                sp = sp.Where(p => !string.IsNullOrEmpty(p.tensanpham) && p.tensanpham.ToLower().Contains(keyword.ToLower()))
+                    .ToList();
+            }
+            return View(sp.OrderBy(n => n.ID_SP).ToPagedList(pageNumber, itemsPerPage));
         }
     }
 }
---
+++ WebOnlineStore/WebOnlineStore/Models/TAIKHOAN.cs
@@ -0,0 +1,52 @@
+namespace WebOnlineStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("TAIKHOAN")]
+    public partial class TAIKHOAN
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public TAIKHOAN()
+        {
+            BINHLUANs = new HashSet<BINHLUAN>();
+            PHANQUYENs = new HashSet<PHANQUYEN>();
+        }
+
+        [Key]
+        public int ID_TK { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string tentk { get; set; }
+
+        [Required]
+        [StringLength(16)]
+        public string phone { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string mail { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string diachi { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<BINHLUAN> BINHLUANs { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PHANQUYEN> PHANQUYENs { get; set; }
+    }
+}
